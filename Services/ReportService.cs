6503f10f95fe99d3904c2d72cff99e88affdeb70
Bulat Gafurov <adlordy@outlook.com>
2016-09-29 23:19:53 +0200

    Rounding adjusted seconds. Closed #1, #2

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using ReportGenerator.Models;
@@ -31,9 +32,19 @@ namespace ReportGenerator.Services
             if (nonPersonal.Any()){
                 var total = nonPersonal.Sum(r=>r.Seconds);
                 var factor = total / (3600 * 8);
+                
                 var values = nonPersonal.Select(r=>{
-                    return (r.Seconds / factor) / 800;
-                });
+                    var value = (r.Seconds / factor) / 900;
+                    return new {Value = value, Decimal = value - Math.Floor(value), Item = r};
+                }).OrderByDescending(v=>v.Decimal).ToList();
+                var count = (4 * 8) - values.Sum(v=>Math.Floor(v.Value));
+                
+                for(var i=0;i<values.Count;i++){
+                    double value = (i<count)?
+                        Math.Ceiling(values[i].Value): 
+                        Math.Floor(values[i].Value);
+                    values[i].Item.AdjustedSeconds = value * 900;
+                }
             }
             return inputList;
         }
